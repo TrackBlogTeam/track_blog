34075e686568e305d8db9e8abcc57943a748cf9a
littlebabyyoung <2537496197@qq.com>
2018-07-16 15:20:13 +0800

    创建了user, administrator, article, writes四个表格

--- back/create_tables.php
+++ back/create_tables.php
@@ -10,6 +10,46 @@ require_once("secret.php");
 
 try {
     $pdo = new PDO("mysql:host=$server;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // creation of table user
+    $sql = "CREATE TABLE user (" .
+        "user_id INT(5) NOT NULL AUTO_INCREMENT," .
+        "user_name VARCHAR(30) NOT NULL," .
+        "user_password VARCHAR(30) NOT NULL," .
+        "phone_number VARCHAR(30) NOT NULL," .
+        "PRIMARY KEY(user_id)," .
+        "UNIQUE(user_name)" .
+        ");";
+    $pdo->exec($sql);
+
+    // creation of table administrator
+    $sql = "CREATE TABLE administrator(" .
+        "administrator_id TINYINT NOT NULL AUTO_INCREMENT," .
+        "administrator_name VARCHAR(30) NOT NULL," .
+        "administrator_password VARCHAR(30) NOT NULL," .
+        "PRIMARY KEY(administrator_id)" .
+        ");";
+    $pdo->exec($sql);
+
+    // creation of table article
+    $sql = "CREATE TABLE article(" .
+        "article_id INT(5) NOT NULL AUTO_INCREMENT," .
+        "author_name VARCHAR(30) NOT NULL," .
+        "article_key VARCHAR(15) NOT NULL," .
+        "PRIMARY KEY(article_id)," .
+        "FOREIGN KEY(author_name) REFERENCES user(user_name)" .
+        ");";
+    $pdo->exec($sql);
+
+    // creation of table writes
+    $sql = "CREATE TABLE writes(" .
+        "article_id INT(5) NOT NULL, " .
+        "author_id INT(5) NOT NULL, " .
+        "FOREIGN KEY(article_id) REFERENCES article(article_id)," .
+        "FOREIGN KEY(author_id) REFERENCES user(user_id)" .
+        ");";
+    $pdo->exec($sql);
 }
 catch (Exception $e) {
     echo $e->getMessage();
