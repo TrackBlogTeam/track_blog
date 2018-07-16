2a6d34a5a3147e30e16a3b24ec533c411c0d1084
littlebabyyoung <2537496197@qq.com>
2018-07-16 11:37:09 +0800

    创建pdo对象

--- back/create_tables.php
+++ back/create_tables.php
@@ -8,4 +8,12 @@
 
 require_once("secret.php");
 
+try {
+    $pdo = new PDO("mysql:host=$server;dbname=$dbname", $username, $password);
+}
+catch (Exception $e) {
+    echo $e->getMessage();
+}
+$pdo = null;
+
 ?>
