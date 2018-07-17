0fa3cc06611f89cf00394e4a424625a902e5d5b3
littlebabyyoung <2537496197@qq.com>
2018-07-17 13:07:27 +0800

    创建DatabaseManager类

---
+++ back/DatabaseManager.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator: littlebabyyoung
+ * Date: 7/17/2018
+ * Time: 1:04 PM
+ */
+
+class DatabaseManager
+{
+
+    public function __construct()
+    {
+
+    }
+}
