6d3596d8271bf1997884ca5aa97ed8d39802537a
littlebabyyoung <2537496197@qq.com>
2018-07-18 20:26:38 +0800

    将DatabaseManager设置为单例模式

--- back/DatabaseManager.php
+++ back/DatabaseManager.php
@@ -8,14 +8,19 @@
 
 class DatabaseManager
 {
+    static private $databaseManger;
+
     private $server;
     private $username;
     private $password;
     private $dbname;
+
     private $pdo;
     private $result;
 
-    public function __construct()
+    // 防止直接创建对象
+    // Prevent directly creating an object
+    private function __construct()
     {
         require_once("secret.php");
         $this->server = $server;
@@ -23,6 +28,27 @@ class DatabaseManager
         $this->password = $password;
         $this->dbname = $dbname;
         $this->pdo = null;
+    }
+
+    // 防止克隆对象
+    // Prevent cloning of this object
+    private function __clone()
+    {
+    }
+
+    // 这个必须是静态函数，否则获取必须外部先new一个DatabaseManager，
+    // 如果外部new出来的话，那这个就不是单例模式了
+    // This function must be a static function, otherwise
+    static public function getDatabaseManger()
+    {
+        // self是指向该类的指针，而this是指向该对象的指针
+        // "self" is the pointer pointing to this class, while "this" is the pointer pointing to this object
+        if (!self::$databaseManger instanceof self) {
+            self::$databaseManger = new self();
+        }
+        return self::$databaseManger;
+        // 现在是在该类的作用域内，访问什么数据都是可取的
+        // Now it is in the domain of this class. Access is available to whichever data.
     }
 
     public function execute($sql)
