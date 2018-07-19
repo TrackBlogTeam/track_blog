d707868775d551ca25f35c6e48d93c7cf7de04ed
littlebabyyoung <2537496197@qq.com>
2018-07-19 23:49:38 +0800

    重新整理了后台代码结构和调整职责分配，添加文件夹api_documents存放api文档

--- back/Controller.php
+++ back/Controller.php
@@ -8,7 +8,7 @@
 
 require_once("DatabaseManager.php");
 
-class Controller
+abstract class Controller
 {
     protected $databaseManager;
 
@@ -24,5 +24,36 @@ class Controller
     public function getDatabaseManager()
     {
         return $this->databaseManager;
+    }
+
+    abstract public function getOwnTable();
+
+    public function getTable($tableName)
+    {
+        if ($_SESSION["role"] != "administrator") {
+            return null;
+        }
+        $sql = "SELECT * FROM $tableName;";
+        $this->databaseManager->execute($sql);
+        $databaseResults = $this->databaseManager->getResult();
+        if (count($databaseResults) > 0) {
+            $tableHead = [];
+            $tableData = [];
+            foreach ($databaseResults[0] as $key => $value) {
+                array_push($tableHead, $key);
+            }
+            foreach ($databaseResults as $record) {
+                $singleData = [];
+                foreach ($record as $key => $value) {
+                    array_push($singleData, $value);
+                }
+                array_push($tableData, $singleData);
+            }
+            $table = new stdClass();
+            $table->head = $tableHead;
+            $table->data = $tableData;
+            return $table;
+        }
+        return null;
     }
 }
