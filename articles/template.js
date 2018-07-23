37dfd132c41f4dd3637c2487a28aeda9891c07af
littlebabyyoung <2537496197@qq.com>
2018-07-23 11:53:50 +0800

    Add local css and js to directory "articles". Update the template html and js.

---
+++ articles/template.js
@@ -0,0 +1,26 @@
+let divElement = document.getElementsByTagName("div")[0]
+
+
+marked.setOptions({
+    renderer: new marked.Renderer(),
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: false,
+    smartLists: true,
+    smartypants: false,
+    highlight: function (code)
+    {
+        return hljs.highlightAuto(code).value;
+    }
+});
+
+console.log(divElement.innerText)
+console.log(divElement.innerHTML)
+let regularExpression = new RegExp("\&lt;", "g")
+let string = divElement.innerHTML.replace(regularExpression, "<")
+regularExpression = new RegExp("\&gt;", "g")
+string = string.replace(regularExpression, ">")
+console.log(string)
+divElement.innerHTML = marked(string, {breaks: true})
