4e8b8624be5c89a52138c2ad623a5e9fbaaf1796
littlebabyyoung <2537496197@qq.com>
2018-07-24 00:04:50 +0800

    Modify ArticleController to adjust to two different types of articles

--- back/ArticleController.php
+++ back/ArticleController.php
@@ -27,24 +27,59 @@ class ArticleController extends Controller
         return parent::getTable(self::tableName);
     }
 
-    public function addArticle($user, $content)
+    public function addArticle($user, $title, $content, $articleType)
     {
+        $title = $this->filterScript($title);
+        $content = $this->filterScript($content);
+        $title = htmlspecialchars($title);
+        $content = htmlspecialchars($content);
+
         //TODO: Optimize the efficiency of the query
         $sql = "SELECT * FROM article WHERE author_name='$user->username';";
         $this->databaseManager->execute($sql);
-        $articlePersonalID = $this->count($this->databaseManager->getResult());
+        $articlePersonalID = count($this->databaseManager->getResult());
         $articleKey = $this->generateKey($articlePersonalID);
-        $sql = "INSERT INTO article (author_name, article_key) VALUES('$user->username', '$articleKey');";
-        $this->databaseManager->execute($sql);
+
+
         try {
-            $path = "../articles/" . $user->username . "/" . $articleKey . ".html";
-            $file = fopen($path, 'w');
-            fwrite($file, $content);
+            //TODO: Discard the string with "<script></script>"
+            // TODO: Use angle brackets to replace double braces
+            $templatePath = null;
+            if ($articleType == "markdown") {
+                $templatePath = dirname(__DIR__) . "/articles/" . "template_markdown.html";
+            }
+            else if ($articleType == "richText") {
+                $templatePath = dirname(__DIR__) . "/articles/" . "template_richText.html";
+            }
+            else {
+                return false;
+            }
+
+            $templateFile = fopen($templatePath, 'r');
+            $templateFileSize = filesize($templatePath);
+            $templateString = fread($templateFile, $templateFileSize);
+
+            echo "The content is: ";
+            var_dump($content);
+            echo "The template is: ";
+            var_dump($templateString);
+            $templateString = str_replace("<articleTitle></articleTitle>", $title . " - 轨迹博客", $templateString);
+            $articleString = str_replace("<content></content>", $content, $templateString);
+            echo "The result is: ";
+            var_dump($articleString);
+
+            $articleFilePath = dirname(__DIR__) . "/articles/" . $user->username . "/" . $articleKey . ".html";
+            $articleFile = fopen($articleFilePath, 'w');
+            fwrite($articleFile, $articleString);
         }
         catch (Exception $e) {
             echo $e->getMessage();
         }
-        fclose($file);
+
+        $sql = "INSERT INTO article (author_name, article_key) VALUES('$user->username', '$articleKey');";
+        $this->databaseManager->execute($sql);
+        fclose($templateFile);
+        fclose($articleFile);
         return $this->databaseManager->getResult();
     }
 
@@ -56,6 +91,11 @@ class ArticleController extends Controller
             $tempID = intval($tempID / 10);
             ++$count;
         }
-        return $articlePersonalID . rand(pow(10, 7 - $count), pow(10, 8 - $count) - 1);
+        return $articlePersonalID . "-" . rand(pow(10, 6 - $count), pow(10, 7 - $count) - 1);
+    }
+
+    private function filterScript($str)
+    {
+        return str_replace("<script>", "", $str);
     }
 }
