c096a432cda63b84f23a4150b1d72907a3631409
littlebabyyoung <2537496197@qq.com>
2018-07-24 21:33:59 +0800

    Change the contents of the template files

--- articles/template_richText.js
+++ articles/template_richText.js
@@ -1,25 +1,5 @@
-let divElement = document.getElementsByTagName("div")[0]
+let divElement = document.getElementsByTagName("div")[0];
 
-
-marked.setOptions({
-    renderer: new marked.Renderer(),
-    gfm: true,
-    tables: true,
-    breaks: false,
-    pedantic: false,
-    sanitize: false,
-    smartLists: true,
-    smartypants: false,
-    highlight: function (code)
-    {
-        return hljs.highlightAuto(code).value;
-    }
-});
-
-console.log(divElement.innerHTML)
-let regularExpression = new RegExp("\&lt;", "g")
-let string = divElement.innerHTML.replace(regularExpression, "<")
-regularExpression = new RegExp("\&gt;", "g")
-string = string.replace(regularExpression, ">")
-console.log(string)
-divElement.innerHTML = marked(string, {breaks: true})
+// 这操作是不是很骚
+divElement.innerHTML = divElement.innerText;
+divElement.innerHTML = divElement.innerText;
