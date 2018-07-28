a873bd7996d7ccb6bef6c81159cb22a168154d5c
luo <1650202750@qq.com>
2018-07-28 16:12:59 +0800

    has an errror and needs help

--- front/me_luo/js/function.js
+++ front/me_luo/js/function.js
@@ -1,21 +1,25 @@
 
 $(function(){
-	var Box="box_2";
-	var Title="C++的4种强制类型转换";
-	var Href="https://blog.csdn.net/cztqwan/article/details/80267691";
-	var Content="一、4种强制类型转换C++不是类型安全的，C++有4种强制类型转换，分别为：static_castdynamic_castconst_castreinterpret_cast二、static_cast（编译时类型检查）主要用法：";
+	var box="box_2";
+	// var title="C++的4种强制类型转换";
+	htmlobj=$.ajax({url:"1.txt",async:false});
+	alert(htmlobj.responseText);
+	var title=htmlobj.responseText;
+	var href="https://blog.csdn.net/cztqwan/article/details/80267691";
+	var content="一、4种强制类型转换C++不是类型安全的，C++有4种强制类型转换，分别为：static_castdynamic_castconst_castreinterpret_cast二、static_cast（编译时类型检查）主要用法：";
 	var _date="2018/7/23";
-	var Time="18:04";
-	var Read="阅读量:234";
-	var Comment="评论：7";
-	$('.firstInfo').append("<div class='box' id="+Box+"></div>");
-	$('#'+Box).append(" <div class='boxA'></div><div class='boxB'></div> ");
-	$('#'+Box+' .boxB').append("<a class='title' href="+Href+" target='_blank'>"+Title+"</a>");
-	$('#'+Box+' .boxB').append("<p class='content'>"+Content+"</p>");
-	$('#'+Box+' .boxB').append("<p class='detail' id='date'>"+_date+"</p>");
-	$('#'+Box+' .boxB').append("<p class='detail' id='time'>"+Time+"</p>");
-	$('#'+Box+' .boxB').append("<p class='detail' id='read'>"+Read+"</p>");
-	$('#'+Box+' .boxB').append("<p class='detail' id='comment'>"+Comment+"</p>");
+	var time="18:04";
+	var read=234;
+	var comment=7;
+	$('.firstInfo').append("<div class='box' id="+box+"></div>");
+	$('#'+box).append(" <div class='boxA'></div><div class='boxB'></div> ");
+	$('#'+box+' .boxB').append("<a class='title' href="+href+" target='_blank'>"+title+"</a>");
+	$('#'+box+' .boxB').append("<p class='content'>"+content+"</p>");
+	$('#'+box+' .boxB').append("<p class='detail' id='date'>"+_date+"</p>");
+	$('#'+box+' .boxB').append("<p class='detail' id='time'>"+time+"</p>");
+	$('#'+box+' .boxB').append("<p class='detail' id='read'>阅读量:"+read+"</p>");
+	$('#'+box+' .boxB').append("<p class='detail' id='comment'>评论:"+comment+"</p>");
+
 	$(".container>div").hide();
 	$(".container>div:eq(0)").show();	
 	
@@ -25,7 +29,7 @@ $(function(){
 		$(".container>div").hide();
 		$(".container>div:eq("+n+")").show();	
 	})
-	$('.boxA,.content').click(function () {
+	$('.boxA,.content,.detail').click(function () {
           window.open('https://blog.csdn.net/cztqwan/article/details/80267691','_blank');  
     });
     $('.write').click(function () {
