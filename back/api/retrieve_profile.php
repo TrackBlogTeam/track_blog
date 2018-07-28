4922e85a462b4fade5a7960fbc43a82ce34b0968
littlebabyyoung <2537496197@qq.com>
2018-07-28 23:51:01 +0800

    Add an API to retrieve a user's profile

--- back/api/retrieve_profile.php
+++ back/api/retrieve_profile.php
@@ -5,3 +5,37 @@
  * Date: 7/21/2018
  * Time: 2:34 PM
  */
+
+require_once("../Util.php");
+require_once("../AdministratorController.php");
+require_once("../UserController.php");
+
+session_start();
+
+if (!isset($_SESSION["username"]) || !isset($_SESSION["role"])) {
+    Util::EndWithCode(835);
+}
+
+// administrator's profile
+if ($_SESSION["role"] == "administrator") {
+    $administratorController = new AdministratorController();
+    // TODO: Improve the table of administrator to contain more information
+    $messageBack = new stdClass();
+    $messageBack->username = $_SESSION["username"];
+    $messageBack->portraitUrl = $_SESSION["portraitUrl"];
+    echo json_encode($messageBack);
+}
+
+// user's profile
+else if ($_SESSION["role"] == "user") {
+    $userController = new UserController();
+    // TODO: Improve the table of administrator to contain more information
+    $messageBack = new stdClass();
+    $messageBack->username = $_SESSION["username"];
+    echo json_encode($messageBack);
+}
+
+// unknown role
+else {
+    Util::EndWithCode(836);
+}
