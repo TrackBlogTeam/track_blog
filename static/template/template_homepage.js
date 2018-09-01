745ff6dfbd066aaa77942de5f50959056963fadc
littlebabyyoung <2537496197@qq.com>
2018-09-01 20:19:00 +0800

    Add HTMLDecode to edit

--- static/template/template_homepage.js
+++ static/template/template_homepage.js
@@ -84,34 +84,4 @@ const app = new Vue({
 });
 
 app.updateSigned();
-
 app.retrieveArticles();
-
-function retrieveArticle()
-{
-    ajax({
-        url: "https://www.track-blog.com/back/api/retrieve_articles.php",
-        method: "POST",
-        success: ((response) =>
-        {
-            console.log(response);
-            const articles = JSON.parse(response);
-            const mainDIV = document.getElementById("main");
-            for (let i = 0; i < articles.length; ++i) {
-                const articleDIV = document.createElement("div");
-                const articleTitleDIV = document.createElement("div");
-                const articleTimeDIV = document.createElement("div");
-                articleDIV.setAttribute("class", "article rounded d-flex justify-content-between align-items-center");
-                const url = "https://www.track-blog.com/users/" + articles[i].author_name + "/articles/" + articles[i].article_key + ".html";
-                articleDIV.setAttribute("onclick", "jumpTo('" + url + "')");
-                articleTitleDIV.setAttribute("class", "articleTitle");
-                articleTimeDIV.setAttribute("class", "articleTime");
-                articleTitleDIV.innerHTML = articles[i].article_title;
-                articleTimeDIV.innerHTML = articles[i].created_time;
-                articleDIV.appendChild(articleTitleDIV);
-                articleDIV.appendChild(articleTimeDIV);
-                mainDIV.appendChild(articleDIV);
-            }
-        })
-    })
-}
